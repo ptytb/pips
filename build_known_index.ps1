b14e2a6d820a16704fbde8703cff620f971b78cd
Ilya Pronin <a@b.cc>
2018-02-17 17:24:30 +0300

    Known packages index now 40K of 130K

--- build_known_index.ps1
+++ build_known_index.ps1
@@ -4,16 +4,26 @@ $env:LC_CTYPE="utf-8"
 [Void][Reflection.Assembly]::LoadWithPartialName("System.Collections")
 [Void][Reflection.Assembly]::LoadWithPartialName("System.Collections.Hashtable")
 [Void][Reflection.Assembly]::LoadWithPartialName("System.IO.FileStream")
-
+[Void][Reflection.Assembly]::LoadWithPartialName("System.Web")
+[Void][Reflection.Assembly]::LoadWithPartialName("System.Web.HttpUtility")
 
 $index = New-Object System.Collections.Hashtable
 $r = [regex] '^(?<Name>.*?)\s*\((?<Version>.*?)\)\s+-\s+(?<Description>.*?)$'
 $count = 0
+$tags_url = 'https://pypi.python.org/pypi?%3Aaction=list_classifiers'
+
+$webClient = New-Object System.Net.WebClient
+$tags = $webClient.DownloadString($tags_url)
 
 # search for a..z as long as `pip search *' does not work; still not the whole list, unfortunately
 
-foreach ($l in [char]'a'..[char]'z') {
-    $letter = [char] $l
+foreach ($l in @([char]'a'..[char]'z'; [char]'0'..[char]'9'; $tags)) {
+    if ($l.GetType() -eq [System.Int32]) {
+        $letter = [char] $l
+    } else {
+        $letter = "$l"
+    }
+    
     Write-Host "Getting $letter"
     $output = & pip search $letter
     
@@ -36,3 +46,5 @@ $fs = New-Object System.IO.FileStream 'known-packages.bin', ([System.IO.FileMode
 $bf = New-Object System.Runtime.Serialization.Formatters.Binary.BinaryFormatter
 $bf.Serialize($fs, $index)
 $fs.Close()
+
+Write-Host "Acquired $count records."
